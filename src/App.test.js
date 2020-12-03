88c31347625334f20c5e8b2ff8e3d5ae93ec5202
Dongyoung <dkim4741@gmail.com>
2020-12-02 23:22:00 -0800

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,13 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const { getByText } = render(<App />);
+  const linkElement = screen.getByText(/Team 12/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = screen.getByText(/Team 12/i);
   expect(linkElement).toBeInTheDocument();
 });
